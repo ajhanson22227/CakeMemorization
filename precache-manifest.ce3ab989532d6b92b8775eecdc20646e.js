dc830caa78fc1644613976d87f811582af1ba864
ajhanson22227 <ajhanson22227@gmail.com>
2020-10-04 14:42:32 -0500

    Updates

---
+++ precache-manifest.ce3ab989532d6b92b8775eecdc20646e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "893cb81cfc8d682196f451938f21effd",
+    "url": "/CakeMemorization/index.html"
+  },
+  {
+    "revision": "86e421df38b673d4992e",
+    "url": "/CakeMemorization/static/css/main.ed38dd3c.chunk.css"
+  },
+  {
+    "revision": "02801dc4b3edfffda189",
+    "url": "/CakeMemorization/static/js/2.ca789f28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CakeMemorization/static/js/2.ca789f28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86e421df38b673d4992e",
+    "url": "/CakeMemorization/static/js/main.e3b3df23.chunk.js"
+  },
+  {
+    "revision": "8ba0403384928457da64",
+    "url": "/CakeMemorization/static/js/runtime-main.bd7fbddb.js"
+  }
+]);
