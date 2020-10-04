afd53f61b4f69bb5e1478cbf24a15f7065aec51d
ajhanson22227 <ajhanson22227@gmail.com>
2020-10-04 14:34:28 -0500

    Add Title and Scores

--- src/components/TitleBar.js
+++ src/components/TitleBar.js
@@ -5,6 +5,7 @@ const TitleBar = (props) => {
 
     return (
         <div className="score-container">
+            <div style={{"fontSize":"32px"}}>Cake Memory</div>
             <div>Current Score: {props.score}</div>
             <div>High Score: {props.bestScore}</div>
         </div>
